048afe323d37a5846cdd15f3804b1d1676cf368b
Dominic-Hulings <dominichulings@gmail.com>
2024-10-29 08:05:36 -0400

    Amost got TableDealer to work, just a few more methods to code

--- src/game.cpp
+++ src/game.cpp
@@ -5,6 +5,21 @@
 
 using std::string;
 
+/*
+private class Table::TableDealer
+{
+  public:
+    Dealer();
+    std::string GETButtonPositions();
+    void preHandCheck();
+    std::stack<std::pair<std::string, std::string>> DealerDeck;
+  private:
+    int Hand(bool isTime);
+    void SETButtonPositions(int dPos, int lbPos, int bbPos);
+    int buttonPositions[3];
+}
+*/
+
 Table::Table(int inMaxPlayers, string inTableName, bool isSave)
 {
   maxPlayers = inMaxPlayers;
@@ -14,7 +29,7 @@ Table::Table(int inMaxPlayers, string inTableName, bool isSave)
 
 void Table::NewHand()
 {
-  
+  this->TableDealer.preHandCheck();
 }
 
 int Table::GETplayerCount()
